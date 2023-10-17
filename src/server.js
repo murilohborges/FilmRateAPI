f37aad759d5032c33f03d6d64daf8c0f77e8010d
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-16 21:02:09 -0300

    first commit with node and express installed

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 3333;
+const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
