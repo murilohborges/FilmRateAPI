2964d6b833be574d1daf1e09c62096e76f8efb23
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-17 19:32:44 -0300

    correcting UserController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -4,10 +4,6 @@ class UsersController {
 
   async create (request, response) {
     const { name, email, password } = request.body;
-    
-    if(!name) {
-      throw new AppError("Nome é obrigatório");
-    }
 
     response.json({ name, email, password });
   }
