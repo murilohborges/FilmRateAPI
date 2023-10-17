507d4b93dc78a6364604b5033598f0d42c1540bc
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-17 19:01:00 -0300

    create folder utils, setting it and server file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 
-const routes = require('./routes')
+const routes = require('./routes');
+
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
 const PORT = 3333;
 
