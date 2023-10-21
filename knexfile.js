fa53d5373bae364f86cb5bba7ba9a6820de20123
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-21 12:43:14 -0300

    setting knexfile and creating knex folder with index file for connect knex to database

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,11 @@
-module.exports = {
+const path = require("path");
 
+module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: path.resolve(__dirname, "src", "database", "database.db")
     },
     useNullAsDefault: true
   }
-
 };
