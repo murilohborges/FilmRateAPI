e7fb5c41766d68220893be915e3a1edca4aef3e9
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-24 19:00:15 -0300

    correcting migrations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
 const usersRouter = require('./user.routes.js');
+const notesRouter = require('./noutes.routes.js');
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/notes", notesRouter);
 
 module.exports = routes;
