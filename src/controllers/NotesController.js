793e7b56f479365f24ad5573ff05e694c666c094
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-24 19:27:29 -0300

    add http request to show a note with its tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -32,7 +32,18 @@ class NotesController{
     
   }
 
-  
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex("movie_notes").where({ id }).first();
+    const tags = await knex("movie_tags").where({ note_id: id }).orderBy("name");
+    
+    return response.json({
+      ...note,
+      tags
+    })
+
+  }
 
 }
 
