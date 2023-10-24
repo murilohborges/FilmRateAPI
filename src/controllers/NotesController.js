2d5941e458cb31cdb76fcc061d92adf4f715f465
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-24 19:09:57 -0300

    adding restriction for rating of films

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,10 +1,15 @@
 const { response } = require("express");
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class NotesController{
   async create(request, response) {
     const { title, description, rating, tags} = request.body;
     const { user_id } = request.params;
+
+    if(rating < 1 || rating > 5) {
+      throw new AppError("A nota da avaliação do filme deve estar entre 1 e 5!")
+    }
 
     const [note_id] = await knex("movie_notes").insert({
       title,
