c09bf79d6cb0c7879d228d0f553cc5f13e4f133e
Murilo Borges <mhenrique.mb@gmail.com>
2023-10-27 19:00:05 -0300

    adding inner join method

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -57,6 +57,7 @@ class NotesController{
     const { title, user_id, tags } = request.query;
 
     let notes;
+    
 
     if(tags) {
       
@@ -69,11 +70,23 @@ class NotesController{
       ]).where("movie_notes.user_id", user_id).whereLike("title", `%${title}%`).whereIn("name", filterTags).innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id");
 
     } else {
-
       notes = await knex("movie_notes").where({ user_id }).whereLike("title", `%${title}%`).orderBy("title");
     }
 
-    return response.json(notes);
+    console.log(notes)
+    
+    const userTags = await knex("movie_tags").where({ user_id });
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id);
+      
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    });
+
+    return response.json(notesWithTags);
     
   }
 
