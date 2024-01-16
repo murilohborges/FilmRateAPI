74cf2aea07486a3cb5faf4fe8b4bc9c79907b475
Murilo Borges <mhenrique.mb@gmail.com>
2024-01-16 12:07:39 -0300

    inserting ensureAuthenticated in UsersController and user.routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 
 const UsersController = require("../controllers/UsersController.js");
+const ensureAutheticated = require("../middleware/ensureAuthenticated.js");
 
 const usersController = new UsersController;
 
 const usersRoutes = Router();
 
 usersRoutes.post("/", usersController.create);
-usersRoutes.put("/:id", usersController.update);
+usersRoutes.put("/", ensureAutheticated, usersController.update);
 
 module.exports = usersRoutes;
