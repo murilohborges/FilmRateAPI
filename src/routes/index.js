ed8f18ac8c1b8526bf629136213e5280cef62c64
Murilo Borges <mhenrique.mb@gmail.com>
2024-01-16 10:56:20 -0300

    Creating SessionsController, sessions.routes and editing index of routes to authentication flow

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,12 @@ const { Router } = require("express");
 const usersRouter = require('./user.routes.js');
 const notesRouter = require('./noutes.routes.js');
 const tagsRouter = require('./tags.routes.js');
+const sessionsRouter = require("./sessions.routes.js");
 
 const routes = Router();
 
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
 routes.use("/notes", notesRouter);
 routes.use("/tags", tagsRouter);
 
