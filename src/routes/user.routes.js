e2745e3bbe7c01f1f5129ae9735b6aa6df875f95
Murilo Borges <mhenrique.mb@gmail.com>
2024-01-17 11:40:22 -0300

    creating UserAvatarController to upload avatar files and save in database, setting userRoutes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,19 +3,18 @@ const multer = require("multer");
 const uploadConfig = require("../config/upload.js");
 
 const UsersController = require("../controllers/UsersController.js");
+const UserAvatarController = require("../controllers/UserAvatarController.js");
 const ensureAutheticated = require("../middleware/ensureAuthenticated.js");
 
 const usersController = new UsersController;
 const upload = multer(uploadConfig.MULTER);
+const userAvatarController = new UserAvatarController();
 
 
 const usersRoutes = Router();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAutheticated, usersController.update);
-usersRoutes.patch("/avatar", ensureAutheticated, upload.single("avatar"), (request, response) => {
-  console.log(request.file.filename);
-  response.json();
-})
+usersRoutes.patch("/avatar", ensureAutheticated, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
